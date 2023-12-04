b8de886a71b2454cf10d79bb8135e634105b9163
matisosagit <149843033+matisosagit@users.noreply.github.com>
2023-12-04 18:46:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const nav = document.querySelector("#barra");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+
+abrir.addEventListener("click", () => {
+    barra.classList.add("visible");
+})
+
+cerrar.addEventListener("click", () => {
+    barra.classList.remove("visible");
+})
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_q4bit5q';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});

b531b33e2dab4b42223a9b0c64ae68d6ea5a7229
matisosagit <149843033+matisosagit@users.noreply.github.com>
2023-12-10 22:10:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -13,17 +13,21 @@ cerrar.addEventListener("click", () => {
 
 const btn = document.getElementById('button');
 
-window.onload = function() {
-            document.getElementById('contact-form').addEventListener('submit', function(event) {
-                event.preventDefault();
-                // generate a five digit number for the contact_number variable
-                this.contact_number.value = Math.random() * 100000 | 0;
-                // these IDs from the previous steps
-                emailjs.sendForm('contact_service', 'contact_form', this)
-                    .then(function() {
-                        console.log('SUCCESS!');
-                    }, function(error) {
-                        console.log('FAILED...', error);
-                    });
-            });
-        }
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_e1d5dt2';
+   const templateID = 'template_q4bit5q';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});

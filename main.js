925bd9c8794f54df2d6d853b470c060a42507f39
matisosagit <149843033+matisosagit@users.noreply.github.com>
2023-12-11 10:44:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -11,23 +11,4 @@ cerrar.addEventListener("click", () => {
     barra.classList.remove("visible");
 });
 
-const btn = document.getElementById('button');
 
-document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
-
-   btn.value = 'Sending...';
-
-   const serviceID = 'service_e1d5dt2';
-   const templateID = 'template_q4bit5q';
-
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
-    });
-});

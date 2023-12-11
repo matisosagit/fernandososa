72f568ce6e58007428868574bfe16603285b6916
matisosagit <149843033+matisosagit@users.noreply.github.com>
2023-12-11 10:44:56 -0300

    Create mensaje.js

---
+++ mensaje.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_e1d5dt2';
+   const templateID = 'template_q4bit5q';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
